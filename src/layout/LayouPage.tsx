afbfe63199136515a62c04c2dc0f16c9aaae9c04
Pedro Lucas Santos <85002280+PedroLuc-cpu@users.noreply.github.com>
2024-10-18 17:04:32 -0300

    feito correção de layout

--- src/layout/LayouPage.tsx
+++ src/layout/LayouPage.tsx
@@ -239,9 +239,9 @@ export default function LayoutPage({ children }: PropsWithChildren) {
             </DropdownMenuContent>
           </DropdownMenu>
         </header>
-        <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
+        <>
           {children}
-        </main>
+        </>
       </div>
     </div>
   );
