d1877e40de265f50ca038744a554e01816f412d4
Pedro Lucas Santos <85002280+PedroLuc-cpu@users.noreply.github.com>
2024-10-19 11:39:54 -0300

    confirando o menu do usuário no sidebar

--- src/layout/app-sidebar.tsx
+++ src/layout/app-sidebar.tsx
@@ -19,8 +19,21 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { signOut, useSession } from "next-auth/react";
 
 export function AppSidebar() {
+  const { data: session } = useSession();
+
   return (
     <Sidebar className="w-64 border-r">
       <SidebarContent>
@@ -87,6 +100,38 @@ export function AppSidebar() {
           </SidebarGroupContent>
         </SidebarGroup>
       </SidebarContent>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" size="icon" className="rounded-full">
+            <Avatar className="h-8 w-8">
+              <AvatarImage
+                src={session?.user?.image as string}
+                alt={session?.user?.name as string}
+              />
+              <AvatarFallback>
+                {session?.user?.name?.charAt(0).toUpperCase()}
+              </AvatarFallback>
+            </Avatar>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56" align="end" forceMount>
+          <DropdownMenuLabel className="font-normal">
+            <div className="flex flex-col space-y-1">
+              <p className="text-sm font-medium leading-none">
+                {session?.user?.name}
+              </p>
+              <p className="text-xs leading-none text-muted-foreground">
+                {session?.user?.email}
+              </p>
+            </div>
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Configurações</DropdownMenuItem>
+          <DropdownMenuItem>Apoiar</DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem onClick={() => signOut()}>Sair</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </Sidebar>
   );
 }
