e50c88ea8d7bd5ef89feb8816dd59888f74c3eac
Pedro Lucas Santos <85002280+PedroLuc-cpu@users.noreply.github.com>
2024-10-23 14:11:20 -0300

    feito comentação da rota de cadastro de usuário

--- src/pages/api/user/create-user.ts
+++ src/pages/api/user/create-user.ts
@@ -1,61 +1,61 @@
-import { prisma } from "@/prisma";
-import type { NextApiRequest, NextApiResponse } from "next";
-import bcrypt from "bcryptjs";
+// import { prisma } from "@/prisma";
+// import type { NextApiRequest, NextApiResponse } from "next";
+// import bcrypt from "bcryptjs";
 
-type Data = {
-  message: string;
-};
+// type Data = {
+//   message: string;
+// };
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>,
-) {
-  if (req.method === "POST") {
-    const { name, email, password, confirmedPassword, isNotification, image } =
-      req.body;
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse<Data>,
+// ) {
+//   if (req.method === "POST") {
+//     const { name, email, password, confirmedPassword, isNotification, image } =
+//       req.body;
 
-    // Validação básica
-    if (!email || !password || !confirmedPassword) {
-      return res
-        .status(400)
-        .json({ message: "Email e senhas são obrigatórios" });
-    }
+//     // Validação básica
+//     if (!email || !password || !confirmedPassword) {
+//       return res
+//         .status(400)
+//         .json({ message: "Email e senhas são obrigatórios" });
+//     }
 
-    if (password !== confirmedPassword) {
-      return res.status(400).json({ message: "As senhas não coincidem" });
-    }
+//     if (password !== confirmedPassword) {
+//       return res.status(400).json({ message: "As senhas não coincidem" });
+//     }
 
-    try {
-      // Verifica se o email já está registrado
-      const existingUser = await prisma.user.findUnique({
-        where: { email },
-      });
+//     try {
+//       // Verifica se o email já está registrado
+//       const existingUser = await prisma.user.findUnique({
+//         where: { email },
+//       });
 
-      if (existingUser) {
-        return res.status(400).json({ message: "Email já está registrado" });
-      }
+//       if (existingUser) {
+//         return res.status(400).json({ message: "Email já está registrado" });
+//       }
 
-      const hashedPassword = await bcrypt.hash(password, 10);
+//       const hashedPassword = await bcrypt.hash(password, 10);
 
-      // Cria o usuário
-      const user = await prisma.user.create({
-        data: {
-          name,
-          email,
-          password: hashedPassword, // Idealmente, você deve hashear a senha antes de salvar no banco
-          confirmedPassword: hashedPassword, // Por razões de segurança, isso não deve ser salvo; apenas para validação no código
-          isNotification: isNotification ?? "off",
-          image,
-        },
-      });
+//       // Cria o usuário
+//       const user = await prisma.user.create({
+//         data: {
+//           name,
+//           email,
+//           password: hashedPassword, // Idealmente, você deve hashear a senha antes de salvar no banco
+//           confirmedPassword: hashedPassword, // Por razões de segurança, isso não deve ser salvo; apenas para validação no código
+//           isNotification: isNotification ?? "off",
+//           image,
+//         },
+//       });
 
-      return res.status(201).json({ message: "Usuário criado com sucesso" });
-    } catch (error) {
-      console.error("Erro ao criar usuário:", error);
-      return res.status(500).json({ message: "Erro interno do servidor" });
-    }
-  } else {
-    res.setHeader("Allow", ["POST"]);
-    res.status(405).json({ message: `Método ${req.method} não permitido` });
-  }
-}
+//       return res.status(201).json({ message: "Usuário criado com sucesso" });
+//     } catch (error) {
+//       console.error("Erro ao criar usuário:", error);
+//       return res.status(500).json({ message: "Erro interno do servidor" });
+//     }
+//   } else {
+//     res.setHeader("Allow", ["POST"]);
+//     res.status(405).json({ message: `Método ${req.method} não permitido` });
+//   }
+// }
