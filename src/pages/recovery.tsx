9e60f7a2460e8ca0e34f773ee57492d4afc677fd
PedroLuc-cpu <Pedrolucas_gta2015@hotmail.com>
2024-11-13 01:09:44 -0300

    feat: implementando a redefinição de senha caso usuário esqueça.

--- src/pages/recovery.tsx
+++ src/pages/recovery.tsx
@@ -6,43 +6,40 @@ import { Input } from "@/components/ui/input";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import Link from "next/link";
 import { z } from "zod";
-import { useForm } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
 import { api } from "@/lib/axios";
-import { useState } from "react";
+import { useRouter } from "next/router";
 
 const formSchema = z.object({
   email: z.string().email({ message: "coloque um email válido" }),
 });
 
 export default function Recovery() {
-  const [loading, setLoading] = useState(false);
-
-  const form = useForm<z.infer<typeof formSchema>>({
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       email: "",
     },
   });
 
-  function onSubmit(value: z.infer<typeof formSchema>) {
-    api
-      .post("/user/reset-account", value)
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((error) => {
-        console.log(error);
+  const onSubmit: SubmitHandler<z.infer<typeof formSchema>> = async (
+    data: z.infer<typeof formSchema>
+  ) => {
+    try {
+      await api.post("/user/reset-account", {
+        email: data.email,
       });
-  }
+      await router.push("/recoverycode");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-[#121214] text-white">
@@ -53,31 +50,17 @@ export default function Recovery() {
           </CardTitle>
         </CardHeader>
         <CardContent>
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem className="space-y-2">
-                    <FormLabel>Email</FormLabel>
-                    <FormControl>
-                      <Input
-                        placeholder="seu email"
-                        className="text-white placeholder-gray-400"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <Button className="w-full text-white" disabled={loading}>
-                Recuperar minha senha
-                {loading && <Loader2 className="animate-spin" />}
-              </Button>
-            </form>
-          </Form>
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+            <Input
+              {...register("email")}
+              placeholder="seu email"
+              className="text-white placeholder-gray-400"
+            />
+            <Button className="w-full text-white" disabled={isSubmitting}>
+              Recuperar minha senha
+              {isSubmitting && <Loader2 className="animate-spin" />}
+            </Button>
+          </form>
           <div className="mt-6">
             <Link
               href="/signIn"
